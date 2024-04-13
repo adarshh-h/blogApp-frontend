01eb4db702b22e7b08245b4be771d271cf6bdcfa
adarshh-h <145352114+adarshh-h@users.noreply.github.com>
2024-04-14 04:18:54 +0530

    Update EditPost.js

--- src/pages/EditPost.js
+++ src/pages/EditPost.js
@@ -12,7 +12,7 @@ export default function EditPost() {
   const [redirect, setRedirect] = useState(false);
   
   useEffect(() => {
-    fetch('https://blog-app-six-blush.vercel.app/post/'+id)
+    fetch('https://blogapp-1-3glq.onrender.com/post/'+id)
      .then(response =>{
         response.json().then(postInfo => {
             setTitle(postInfo.title);
@@ -33,7 +33,7 @@ async function updatePost(ev){
         data.set("file", files?.[0]);
     }
 
-    const response = await fetch('https://blog-app-six-blush.vercel.app/post',{
+    const response = await fetch('https://blogapp-1-3glq.onrender.com/post',{
         method:'PUT',
         body:data,
         credentials:'include',
