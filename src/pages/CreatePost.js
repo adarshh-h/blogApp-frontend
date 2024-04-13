50ac37f694070f53ae8e2c985f381a71f7cb4e43
adarshh-h <145352114+adarshh-h@users.noreply.github.com>
2024-04-14 04:18:12 +0530

    Update CreatePost.js

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -20,7 +20,7 @@ export default function CreatePost() {
     data.set("content", content);
     data.set("file", files[0]);
     ev.preventDefault();
-    const response = await fetch("https://blog-app-six-blush.vercel.app/post", {
+    const response = await fetch("https://blogapp-1-3glq.onrender.com/post", {
       method: "POST",
       body: data,
       credentials: "include",
